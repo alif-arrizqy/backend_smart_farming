b560d1fd983d26fb3d4872b2f1d6119df395188a
alif-arrizqy <alif.arrizqy20@gmail.com>
2022-12-11 19:56:43 +0700

    added timeout pymongo

--- server/controllers/temperature/temperature.py
+++ server/controllers/temperature/temperature.py
@@ -6,13 +6,24 @@ from pymongo import MongoClient
 from datetime import datetime
 from server.response_helper import *
 from dotenv import load_dotenv
+from time import sleep
 
 env = load_dotenv()
 
-# mongodb
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client[os.getenv("DATABASE")]
-collection = db.temperature
+# mongodb connection
+# attempt fails
+max_retries = 5
+num_fails = 0
+try:
+    client = MongoClient(os.getenv("MONGO_URI"), serverSelectionTimeoutMS=10000, connectTimeoutMS=10000, maxPoolSize=20, waitQueueTimeoutMS=10000)
+    db = client[os.getenv("DATABASE")]
+    collection = db.temperature
+except Exception as e:
+    num_fails += 1
+    print(f"{e} attempt fail {num_fails}")
+    if num_fails > max_retries:
+        raise e
+    sleep(0.5)
 
 # BASE URL
 base_url = os.getenv("BASE_URL")
